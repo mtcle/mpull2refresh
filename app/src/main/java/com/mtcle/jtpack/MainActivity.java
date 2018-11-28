bf0320fa65869fea59553c55d146b011e1862186
mtcle <mtcle@126.com>
2018-11-28 16:52:21 +0800

    update

---
+++ app/src/main/java/com/mtcle/jtpack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mtcle.jtpack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
